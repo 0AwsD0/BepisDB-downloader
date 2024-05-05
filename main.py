1ed4c2daf045e65d7028b178c7a5b6d5b1786f86
0AwsD0 <64651813+0AwsD0@users.noreply.github.com>
2024-05-05 23:37:25 +0200

    advanced_download() Reday! -> not tested thoroughly | but seems to work just fine

--- main.py
+++ main.py
@@ -22,207 +22,246 @@ def menu():
         print("If you wish to enter only name and/or tags press 1 or ENTER (or anything else really). For advance download press 2.")
         selected = input()
         if selected == "2":
-            KK(game, url)
+            mode = "advanced"
+            KK(game, url, mode)
         else:
-            basic(game, url)
+            mode = "basic"
+            basic(game, url, mode)
     elif selected == "2":
         game = "AA2"
         url = "https://db.bepis.moe/aa2"
         print("If you wish to enter only name and/or tags press 1 or ENTER (or anything else really). For advance download press 2.")
         selected = input()
         if selected == "2":
-            AA2_and_AI_HS2(game, url)
+            mode = "advanced"
+            AA2_and_AI_HS2(game, url, mode)
         else:
-            basic(game, url)
+            mode = "basic"
+            basic(game, url, mode)
     elif selected == "3":
         game = "HS"
         url = "https://db.bepis.moe/honeyselect"
         print("If you wish to enter only name and/or tags press 1 or ENTER (or anything else really). For advance download press 2.")
         selected = input()
         if selected == "2":
-            HS(game, url)
+            mode = "advanced"
+            HS(game, url, mode)
         else:
-            basic(game, url)
+            mode = "basic"
+            basic(game, url, mode)
     elif selected == "4":
         game = "PH"
         url = "https://db.bepis.moe/playhome"
         print("If you wish to enter only name and/or tags press 1 or ENTER (or anything else really). For advance download press 2.")
         selected = input()
         if selected == "2":
-            PH(game, url)
+            mode = "advanced"
+            PH(game, url, mode)
         else:
-            basic(game, url)
+            mode = "basic"
+            basic(game, url, mode)
     elif selected == "5":
         game = "AI_HS2"
         url = "https://db.bepis.moe/aishoujo"
         print("If you wish to enter only name and/or tags press 1 or ENTER (or anything else really). For advance download press 2.")
         selected = input()
         if selected == "2":
-            AA2_and_AI_HS2(game, url)
+            mode = "advanced"
+            AA2_and_AI_HS2(game, url, mode)
         else:
-            basic(game, url)
+            mode = "basic"
+            basic(game, url, mode)
     elif selected == "6":
         game = "COM3D2"
         url = "https://db.bepis.moe/com3d2"
         print("If you wish to enter only name and/or tags press 1 or ENTER (or anything else really). For advance download press 2.")
         selected = input()
         if selected == "2":
-            COM3D2(game, url)
+            mode = "advanced"
+            COM3D2(game, url, mode)
         else:
-            basic(game, url)
+            mode = "basic"
+            basic(game, url, mode)
     elif selected == "7":
         game = "SH"
         url = "https://db.bepis.moe/summerheat"
         print("If you wish to enter only name and/or tags press 1 or ENTER (or anything else really). For advance download press 2.")
         selected = input()
         if selected == "2":
-            SH(game, url)
+            mode = "advanced"
+            SH(game, url, mode)
         else:
-            basic(game, url)
+            mode = "basic"
+            basic(game, url, mode)
     elif selected == "8":
         game = "HC"
         url = "https://db.bepis.moe/honeycome"
         print("If you wish to enter only name and/or tags press 1 or ENTER (or anything else really). For advance download press 2.")
         selected = input()
         if selected == "2":
-            HC(game, url)
+            mode = "advanced"
+            HC(game, url, mode)
         else:
-            basic(game, url)
+            mode = "basic"
+            basic(game, url, mode)
     else:
         print("Please select one of the correct options (1-6):")
         main()
 
 
-def KK(game, url):
+def KK(game, url, mode):
     print("Enter in order (or use config file) the parameters or press enter to set default.")
     print("Name: (text) <Default> = blank")
     name = input()
     print("Tags: (text) <Default> = blank")
     tags = input()
     print("Gender: (text) <Default> = Unspecified")
+    print("Possible Gender: female | male")
     gender = input()
-    print("Personality: (text) <Default> = Unspecified")
+    print("Personality: (number) <Default> = Unspecified")
+    print("Possible Personality: 0 = Sexy | 1 = Ojousama | 2 = Snobby | 3 = Kouhai | 4 = Mysterious | 5 = Weirdo | 6 = Yamato Nadeshiko | 7 = Tomboy | 8 = Pure | 9 = Simple | 10 = Delusional | 11 = Motherly | 12 = Big Sisterly | 13 = Gyaru | 14 = Delinquent | 15 = Wild | 16 = Wannabe | 17 = Reluctant | 18 = Jinxed | 19 = Bookish | 20 = Timid | 21 = Typical Schoolgirl | 22 = Trendy | 23 = Otaku | 24 = Yandere | 25 = Lazy | 26 = Quiet | 27 = Stubborn | 28 = Old-Fashioned | 29 = Humble | 30 = Friendly | 31 = Willful | 32 = Honest | 33 = Glamorous | 34 = Returnee | 35 = Slangy | 36 = Sadistic | 37 = Emotionless | 38 = Perfectionist")
     personality = input()
     print("Gametype: (text) <Default> = Unspecified")
+    print("Possible Gametype: Base | Steam | Steam 18+ | Emotion Creators | Sunshine")
     game_type = input()
-    print("Does bit contain modded content: (checkbox - enter '1' to check - 0 or nothig to leave unchecked <Default>)")
+    print("Does not contain modded content: (checkbox) - enter '1' to check - 0 or nothig to leave unchecked <Default>")
     modded_content = input()
-    print("Order by: (enter 1 for 'Date Descending' <Default> | 2 for 'Date Ascending' | 3 for 'Popularity')")
+    print("Order by: (text) <Default> = Date Descending")
+    print("You can Order by: popularity | dateasc (Date Ascending)")
     order_by = input()
-    print("Show hidden: (checkbox - enter '1' to check - 0 or nothig to leave unchecked <Default>)")
+    print("Show hidden: (checkbox) - enter '1' to check - 0 or nothig to leave unchecked <Default>")
     show_hidden = input()
-    print("Show only featured: (checkbox - enter '1' to check - 0 or nothig to leave unchecked <Default>)")
+    print("Show only featured: (checkbox) - enter '1' to check - 0 or nothig to leave unchecked <Default>")
     show_only_featured = input()
-    download(game = game, url = url, name = name, tags = tags, gender = gender, personality = personality, game_type = game_type, modded_content = modded_content, order_by = order_by, show_hidden = show_hidden, show_only_featured = show_only_featured)
+    download(mode = mode, game = game, url = url, name = name, tags = tags, gender = gender, personality = personality, game_type = game_type, modded_content = modded_content, order_by = order_by, show_hidden = show_hidden, show_only_featured = show_only_featured)
 
 
-def AA2_and_AI_HS2(game, url):
+def AA2_and_AI_HS2(game, url, mode):
     print("Enter in order (or use config file) the parameters or press enter to set default.")
     print("Name: (text) <Default> = blank")
     name = input()
     print("Tags: (text) <Default> = blank")
     tags = input()
     print("Gender: (text) <Default> = Unspecified")
+    if(game == "AA2"):
+        print("Possible Gender: female | male")
+    else:
+        print("Possible Gender: Female | Male | Futanari")
     gender = input()
-    print("Personality: (text) <Default> = Unspecified")
+    print("Personality: (number) <Default> = Unspecified")
+    if(game == "AA2"):
+        print("Possible Personality: 0 = Lively | 1 = Delicate | 2 = Cheerful | 3 = Quiet | 4 = Playful | 5 = Frisky | 6 = Kind | 7 = Joyful | 8 = Ordinary | 9 = Irritated | 10 = Harsh | 11 = Sweet | 12 = Creepy | 13 = Reserved | 14 = Dignified | 15 = Aloof | 16 = Smart | 17 = Genuine | 18 = Mature | 19 = Lazy | 20 = Manly | 21 = Gentle | 22 = Positive | 23 = Otaku | 24 = Savage | 25 = Cadet | 26 = Caring | 27 = Schemer | 28 = Carefree | 29 = Warm | 30 = Cold (Male Aloof) | 31 = AA1 Quiet | 32 = Methodical | 33 = AA1 Empress (HF) | 34 = AA1 Big Sister | 35 = Silent | 36 = AA1 Empress (aa2g) | 37 = AA1 Brave | 38 = AA1 Pure | 40 = Robot | 41 = Sly | 42 = Russian | 43 = Timid | 44 = Whimsical | 45 = Salamanderman | 46 = Callous | 50 = Servent | 51 = OuterSpace | 52 = Apricot | 53 = Wise | 54 = Graceful | 55 = Trendy | 58 = AA1 Ditzy | 59 = AA1 Aloof | 60 = AA1 Affable | 61 = AA1 Calm | 62 = AA1 Tense | 63 = AA1 Rough | 64 = AA1 Sweet | 65 = AA1 Mistress | 66 = AA1 Selfish | 67 = Bekloppt | 68 = AA1 Casual | 69 = Open-eye Joyful | 70 = AA1 Pleasant | 71 = Gynoid (Sweet) | 72 = Open-eye Quiet | 73 = Scholarly | 75 = Cynical | 76 = AA1 Manly | 77 = AA1 Humble | 78 = Succubus (Mature) | 79 = AA1 Violent | 80 = Analytical | 81 = Despair | 82 = Future | 83 = Kuma | 84 = Liar | 85 = Robot (Male) | 90 = Big Brother | 95 = Polite | 96 = Energetic | 97 = Eccentric | 98 = AA1 Cold | 99 = AA1 Soft | 100 = AA1 Eloquent | 101 = AA1 Obscene | 102 = AA1 Faithful | 103 = Professor | 104 = AA1 Handsome | 105 = AA1 Dark | 106 = Twisted | 107 = Cowardly | 108 = Haughty | 110 = Ice | 111 = Hero | 112 = Artsy | 113 = Silent (Male) | 114 = Mute | 115 = Mute+ | 116 = Arrogant | 131 = Bael | 132 = Fumi | 133 = Space Case | 134 = Wild | 135 = Delinquent | 136 = Jinxed | 137 = Chuuni | 138 = Motherly | 139 = Tomboy | 140 = Yandere | 141 = Gyaru | 142 = Bookish | 143 = Archaic | 144 = Enigma | 145 = Kouhai | 146 = Emotionless | 147 = Bashful | 148 = Sadistic | 149 = Geek")
+    else:
+        print("Possible Personality: 0 = Emotionless | 1 = Friendly | 2 = Confident | 3 = Selfish | 4 = Lazy | 5 = Positive")
     personality = input()
-    print("Order by: (enter 1 for 'Date Descending' <Default> | 2 for 'Date Ascending' | 3 for 'Popularity')")
+    print("Order by: (text) <Default> = Date Descending")
+    print("You can Order by: popularity | dateasc (Date Ascending)")
     order_by = input()
-    print("Show hidden: (checkbox - enter '1' to check - 0 or nothig to leave unchecked <Default>)")
+    print("Show hidden: (checkbox) - enter '1' to check - 0 or nothig to leave unchecked <Default>")
     show_hidden = input()
-    print("Show only featured: (checkbox - enter '1' to check - 0 or nothig to leave unchecked <Default>)")
+    print("Show only featured: (checkbox) - enter '1' to check - 0 or nothig to leave unchecked <Default>")
     show_only_featured = input()
-    download(game = game, url = url, name = name, tags = tags, gender = gender, personality = personality, order_by = order_by, show_hidden = show_hidden, show_only_featured = show_only_featured)
+    download(mode = mode, game = game, url = url, name = name, tags = tags, gender = gender, personality = personality, order_by = order_by, show_hidden = show_hidden, show_only_featured = show_only_featured)
 
-def HS(game, url):
+def HS(game, url, mode):
     print("Enter in order (or use config file) the parameters or press enter to set default.")
     print("Name: (text) <Default> = blank")
     name = input()
     print("Tags: (text) <Default> = blank")
     tags = input()
     print("Gender: (text) <Default> = Unspecified")
+    print("Possible Gender: female | male")
     gender = input()
-    print("Personality: (text) <Default> = Unspecified")
+    print("Personality: (number) <Default> = Unspecified")
+    print("Possible Personality: 0 = Cheerful | 1 = Tsundere | 2 = Gentle | 3 = Withdrawn | 4 = Yandere | 5 = Easygoing | 6 = Strict | 7 = Boyish | 8 = Energetic | 9 = Diligent | 10 = Active | 11 = Sincere | 12 = Bewitching")
     personality = input()
     print("Gametype: (text) <Default> = Unspecified")
+    print("Possible Gametype: neo | Classic")
     game_type = input()
-    print("Order by: (enter 1 for 'Date Descending' <Default> | 2 for 'Date Ascending' | 3 for 'Popularity')")
+    print("Order by: (text) <Default> = Date Descending")
+    print("You can Order by: popularity | dateasc (Date Ascending)")
     order_by = input()
-    print("Show hidden: (checkbox - enter '1' to check - 0 or nothig to leave unchecked <Default>)")
+    print("Show hidden: (checkbox) - enter '1' to check - 0 or nothig to leave unchecked <Default>")
     show_hidden = input()
-    print("Show only featured: (checkbox - enter '1' to check - 0 or nothig to leave unchecked <Default>)")
+    print("Show only featured: (checkbox) - enter '1' to check - 0 or nothig to leave unchecked <Default>")
     show_only_featured = input()
-    download(game = game, url = url, name = name, tags = tags, gender = gender, personality = personality, game_type = game_type, order_by = order_by, show_hidden = show_hidden, show_only_featured = show_only_featured)
+    download(mode = mode, game = game, url = url, name = name, tags = tags, gender = gender, personality = personality, game_type = game_type, order_by = order_by, show_hidden = show_hidden, show_only_featured = show_only_featured)
 
 
-def PH(game, url):
+def PH(game, url, mode):
     print("Enter in order (or use config file) the parameters or press enter to set default.")
     print("Name: (text) <Default> = blank")
     name = input()
     print("Tags: (text) <Default> = blank")
     tags = input()
-    print("Order by: (enter 1 for 'Date Descending' <Default> | 2 for 'Date Ascending' | 3 for 'Popularity')")
+    print("Order by: (text) <Default> = Date Descending")
+    print("You can Order by: popularity | dateasc (Date Ascending)")
     order_by = input()
-    print("Show hidden: (checkbox - enter '1' to check - 0 or nothig to leave unchecked <Default>)")
+    print("Show hidden: (checkbox) - enter '1' to check - 0 or nothig to leave unchecked <Default>")
     show_hidden = input()
-    print("Show only featured: (checkbox - enter '1' to check - 0 or nothig to leave unchecked <Default>)")
+    print("Show only featured: (checkbox) - enter '1' to check - 0 or nothig to leave unchecked <Default>")
     show_only_featured = input()
-    download(game = game, url = url, name = name, tags = tags, order_by = order_by, show_hidden = show_hidden, show_only_featured = show_only_featured)
+    download(mode = mode, game = game, url = url, name = name, tags = tags, order_by = order_by, show_hidden = show_hidden, show_only_featured = show_only_featured)
 
-def COM3D2(game, url):
+def COM3D2(game, url, mode):
     print("Enter in order (or use config file) the parameters or press enter to set default.")
     print("Name: (text) <Default> = blank")
     name = input()
     print("Tags: (text) <Default> = blank")
     tags = input()
     print("Preset type: (text) <Default> = Unspecified")
+    print("Possible Preset type: outfit | body | all")
     preset_type = input()
-    print("Order by: (enter 1 for 'Date Descending' <Default> | 2 for 'Date Ascending' | 3 for 'Popularity')")
+    print("Order by: (text) <Default> = Date Descending")
     order_by = input()
-    print("Show hidden: (checkbox - enter '1' to check - 0 or nothig to leave unchecked <Default>)")
+    print("Show hidden: (checkbox) - enter '1' to check - 0 or nothig to leave unchecked <Default>")
     show_hidden = input()
-    print("Show only featured: (checkbox - enter '1' to check - 0 or nothig to leave unchecked <Default>)")
+    print("Show only featured: (checkbox) - enter '1' to check - 0 or nothig to leave unchecked <Default>")
     show_only_featured = input()
-    download(game = game, url = url, name = name, tags = tags, preset_type = preset_type, order_by = order_by, show_hidden = show_hidden, show_only_featured = show_only_featured)
+    download(mode = mode, game = game, url = url, name = name, tags = tags, preset_type = preset_type, order_by = order_by, show_hidden = show_hidden, show_only_featured = show_only_featured)
 
 
-def SH(game, url):
+def SH(game, url, mode):
     print("Enter in order (or use config file) the parameters or press enter to set default.")
     print("Name: (text) <Default> = blank")
     name = input()
     print("Tags: (text) <Default> = blank")
     tags = input()
-    print("Order by: (enter 1 for 'Date Descending' <Default> | 2 for 'Date Ascending' | 3 for 'Popularity')")
+    print("Order by: (text) <Default> = Date Descending")
+    print("You can Order by: popularity | dateasc (Date Ascending)")
     order_by = input()
-    print("Show hidden: (checkbox - enter '1' to check - 0 or nothig to leave unchecked <Default>)")
+    print("Show hidden: (checkbox) - enter '1' to check - 0 or nothig to leave unchecked <Default>")
     show_hidden = input()
-    print("Show only featured: (checkbox - enter '1' to check - 0 or nothig to leave unchecked <Default>)")
+    print("Show only featured: (checkbox) - enter '1' to check - 0 or nothig to leave unchecked <Default>")
     show_only_featured = input()
-    download(game = game, url = url, name = name, tags = tags, order_by = order_by, show_hidden = show_hidden, show_only_featured = show_only_featured)
+    download(mode = mode, game = game, url = url, name = name, tags = tags, order_by = order_by, show_hidden = show_hidden, show_only_featured = show_only_featured)
 
-def HC(game, url):
+def HC(game, url, mode):
     print("Enter in order (or use config file) the parameters or press enter to set default.")
     print("Name: (text) <Default> = blank")
     name = input()
     print("Tags: (text) <Default> = blank")
     tags = input()
     print("Gender: (text) <Default> = Unspecified")
+    print("Possible Gender: Female | Male | Futanari")
     gender = input()
-    print("Order by: (enter 1 for 'Date Descending' <Default> | 2 for 'Date Ascending' | 3 for 'Popularity')")
+    print("Order by: (text) <Default> = Date Descending")
+    print("You can Order by: popularity | dateasc (Date Ascending)")
     order_by = input()
-    print("Show hidden: (checkbox - enter '1' to check - 0 or nothig to leave unchecked <Default>)")
+    print("Show hidden: (checkbox) - enter '1' to check - 0 or nothig to leave unchecked <Default>")
     show_hidden = input()
-    print("Show only featured: (checkbox - enter '1' to check - 0 or nothig to leave unchecked <Default>)")
+    print("Show only featured: (checkbox) - enter '1' to check - 0 or nothig to leave unchecked <Default>")
     show_only_featured = input()
-    download(game = game, url = url, name = name, tags = tags, gender = gender, order_by = order_by, show_hidden = show_hidden, show_only_featured = show_only_featured)
+    download(mode = mode, game = game, url = url, name = name, tags = tags, gender = gender, order_by = order_by, show_hidden = show_hidden, show_only_featured = show_only_featured)
 
-def basic(game, url):
+def basic(game, url, mode):
+    mode = mode
     print("Enter in order (or use config file) the parameters or press enter to set default.")
     print("Name: (text) <Default> = blank")
     name = input()
     print("Tags: (text) <Default> = blank")
     tags = input()
-    download(mode = "basic", game = game, url = url, name=name, tags=tags)
+    download(mode = mode, game = game, url = url, name = name, tags = tags)
 
 def main():
     menu()
--- test2.py
+++ test2.py
@@ -3,139 +3,6 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
 
-#BTW selenium 420 ~blaze it XD
-
-def download(**data):
-    print("Download function invoked with arguments: ", data)
-
-#below code checks if the basic option is selected - if yes -> the universal function will be invoked | if no -> the advanced functions will be invoked - tailored to the data that is for set game type
-    if data["mode"] == "basic":
-        try:
-            basic_download(data["url"], data["name"], data["tags"])
-        except:
-            print("basic_download() - failed")
-            print("Is website down?")
-            print("Did you input proper values?")
-            print("Exiting in 5 seconds.")
-            time.sleep(5)
-            exit(1)
-    else:
-        try:
-            advanced_download(**data)
-        except:
-            print("advanced_download() - failed")
-            print("Is website down?")
-            print("Did you input proper values?")
-            print("Exiting in 5 seconds.")
-            time.sleep(5)
-            exit(1)
-
-
-def basic_download(url, name, tags):
-    print("Basic download function invoked.")
-    #I need to let user config wait time and provide some values or idk explain it in documentation and readme.md
-    #OR
-    #I may try using watchdog or directory scan to check if file exists -> the card and waiting until it does so there is no need for download interval - it downloads only if previous card is downloaded
-    print("WARNING: Some cards weigh a lot, even over 25MB - if your intewrnet is slow CHANGE WAIT TIME between card downloads to even 10 seconds!")
-
-    try:
-        url = url
-        name = name
-        tags = tags
-        print("url: ", url, " name: ", name," tag: ", tags)
-        #selenium code below
-        options = webdriver.FirefoxOptions()
-        #options.add_argument("-headless")
-        #driver = webdriver.Firefox(options=options)
-        driver = webdriver.Firefox()
-        driver.get(url)
-
-        # I know I can just make up the link to the website since it's uses GET method like "https://db.bepis.moe/koikatsu?name=aaaa&tag=bbbb" but I want to try use the way below
-        name_input = driver.find_element(By.ID, "name")
-        name_input.send_keys(name)
-        tag_input = driver.find_element(By.ID, "tag")
-        tag_input.send_keys(tags)
-        tag_input.submit()
-
-        print("Waiting 5 seconds for page to load...")
-        time.sleep(5)
-
-        #just set the loop to be true until the "Next" button is "disabled" than flip the flag to exit loop // if Next button DISABLED flag = 0
-        i = 2
-        i2 = 1
-        flag = 1
-
-        #base url - the url2 is for adding '&page='+i and than reseting it back to normal before adding next &page
-        url = driver.current_url
-    except Exception:
-        print("Exception in basic_download() before loop: " + Exception)
-        print("Exiting in 5 seconds.")
-        time.sleep(5)
-        driver.quit()
-        exit(1)
-
-        while (flag == 1):
-            try:
-                try:
-                    while (i2 <= 24):
-                        print(f"Downloading card number: {i2}")
-                        download_selector = "document.querySelector('#inner-card-body > div:nth-child(" + str(i2) + ") > div > div > a.btn.btn-primary.btn-sm').click();"
-                        time.sleep(4)
-                        driver.execute_script(download_selector)
-                        i2 += 1
-                except Exception:
-                    print("The download FINISHED!")
-                    print("There were less than 24 cards on last page.")
-                    print(">>OR There is slight chance, that website got down.")
-                    #    flag == 0 + go back to main() // or leave like that to exit program
-                    print("Exiting in 5 seconds.")
-                    time.sleep(5)
-                    driver.quit()
-                    exit(0)
-                next_button_script = """
-                xpath = "//a[contains(text(),'Next')]";
-                var matchingElement = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
-                parentDiv = matchingElement.parentNode;
-                if(parentDiv.classList.contains('disabled') == true){
-                    var state = "Disabled";
-                    return state;
-                }
-                else{
-                    var state = "Enabled";
-                    return state;
-                }
-                """
-                print("Getting 'Next' button state...")
-                button_state = driver.execute_script(next_button_script)
-                #sleep for script and mainly for last card/cards to download
-                time.sleep(5)
-                print("Button state = "+button_state)
-                if(button_state == "Disabled"):
-                    print("Download FINISHED!")
-                    #    flag == 0 + go back to main() // or leave like that to exit program
-                    print("-------------------")
-                    print("Exiting in 5 seconds.")
-                    driver.quit()
-                    time.sleep(5)
-                    exit(0)
-                print("Getting url...")
-                url2 = url
-                print("Got url - adding page number...")
-                url2 += "&page=" + str(i)
-                print("Waiting 5 seconds for page to load...")
-                driver.get(url2)
-                time.sleep(5)
-                i2 = 1
-                i += 1
-            except Exception:
-                print("The download failed. //loop "+Exception)
-                print("Exiting in 5 seconds.")
-                time.sleep(5)
-                driver.quit()
-                exit(1)
-
-#I tried to make below shorter since the download LOOP is same as in basic download, but the driver must be initialized in same method, so I can't do it without spawning additional unnecessary browsers
-#But in other end it's easier to deal with code that is simpler, even if there is more of it. ~Terry would approve
 def advanced_download(**data):
     print("Advanced download function invoked.")
     print("DATA: ", data)
@@ -157,7 +24,9 @@ def advanced_download(**data):
     #OR
     #I may try using watchdog or directory scan to check if file exists -> the card and waiting until it does so there is no need for download interval - it downloads only if previous card is downloaded
     print("WARNING: Some cards weigh a lot, even over 25MB - if your intewrnet is slow CHANGE WAIT TIME between card downloads to even 10 seconds!")
-
+    ''' 
+    try:
+    '''
     url = data["url"]
     name = data["name"]
     tags = data["tags"]
@@ -293,9 +162,46 @@ def advanced_download(**data):
             time.sleep(5)
             driver.quit()
             exit(1)
+    ''' 
+    except Exception:
+        print("Exception in advanced_download(): ", Exception)
+        print("Exiting in 5 seconds.")
+        time.sleep(5)
+        driver.quit()
+        exit(1)
 
+'''
 def main():
-    print("download.py")
+    print("test2.py")
 
+    mode = "advanced"
+    game = "KK"
+    url = "https://db.bepis.moe/koikatsu"
+    name = "Ayaka"
+    tags = "Genshin_Impact"
+    gender = ""
+    personality = "1"
+    order_by = "" #<---- "" or 0
+    show_hidden = "1"
+    show_only_featured = ""
+    advanced_download(mode= mode, game=game, url=url, name=name, tags=tags, gender=gender, personality=personality, order_by=order_by, show_hidden=show_hidden, show_only_featured=show_only_featured)
+
+    #The Below Works TOO
+'''
+    mode = "advanced"
+    game = "COM3D2"
+    url = "https://db.bepis.moe/com3d2"
+    name = ""
+    tags = ""
+    preset_type = "body"
+    order_by = ""  # <---- "" or 0
+    show_hidden = "1"
+    show_only_featured = ""
+    advanced_download(mode= mode, game=game, url=url, name=name, tags=tags,preset_type = preset_type, order_by=order_by, show_hidden=show_hidden, show_only_featured=show_only_featured)
+    '''
 if __name__ == "__main__":
     main()
+
+#KEKW - that tests (.py)
+#Do you read a code comments? If yes, then good for you!
+#Should't I assert it?... get it? assert? XDDD
